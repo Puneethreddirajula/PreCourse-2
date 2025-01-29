0aaa3495272a98d6b05e96470a7a91a8467b1ed8
Puneethreddirajula <puneeth.reddirajula@gmail.com>
2025-01-29 18:08:31 -0500

    Completed PreCourse-2

--- Exercise_3.java
+++ Exercise_3.java
@@ -1,4 +1,6 @@
-class LinkedList 
+//Time Complexity: O(n)
+//Space Complexity: O(1)
+class LinkedList
 { 
     Node head; // head of linked list 
   
@@ -20,6 +22,12 @@ class LinkedList
     { 
         //Write your code here
 	//Implement using Fast and slow pointers
+        Node slow = head;
+        Node fast = head;
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
     } 
   
     public void push(int new_data) 
